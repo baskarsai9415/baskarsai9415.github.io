85c95d0c04eca1a20aeae7a174a1fbae0461d619
baskarsai9415 <baskarsai9415@gmail.com>
2022-08-25 22:29:26 +0530

    portfolio

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-const toogleBtn = document.getElementsByClassName('toogle-button')[0]
-const navbarLinks = document.getElementsByClassName('container1')[0]
+const home = document.getElementById("home");
+const contact = document.getElementById("contact");
+const about = document.getElementById("project")
 
-toogleBtn.addEventListener('click',()=>{
-    navbarLinks.classList.toggle('active')
-})
+home.addEventListener("click",homeBtn);
+function homeBtn(){
+  window.open("http://127.0.0.1:5500/index.html")
+};
+
+contact.addEventListener("click",contactBtn);
+function contactBtn(){
+  window.open("http://127.0.0.1:5500/contact.html")
+};
+
+contact.addEventListener("click",aboutBtn);
+function aboutBtn(){
+  window.open("http://127.0.0.1:5500/about.html")
+};
